5a6d153d40c3b7785c3e0d64761a199e2afb1ed9
Paul Moreno <paulmoreno555@gmail.com>
2024-03-27 12:27:04 -0500

    URL

--- Assets/Js/home.js
+++ Assets/Js/home.js
@@ -1,6 +1,8 @@
+const REF_INICIO = "./pages/Inicio.html";
+
 function redirect(num) {
   if (num == 1) {
-    window.location.href = "../../pages/inicio.html";
+    window.location.href = REF_INICIO;
   } else if (num == 2) {
     window.location.href = "../../pages/conocerte.html";
   } else if (num == 3) {
