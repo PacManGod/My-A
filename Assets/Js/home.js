aeb7a8d86ee8d59979f3b1bd95297d1c2a81ecc8
Paul Moreno PC HW <paulmoreno555@gmail.com>
2024-03-27 11:36:54 -0500

    first commit

---
+++ Assets/Js/home.js
@@ -0,0 +1,9 @@
+function redirect(num) {
+  if (num == 1) {
+    window.location.href = "../../pages/inicio.html";
+  } else if (num == 2) {
+    window.location.href = "../../pages/conocerte.html";
+  } else if (num == 3) {
+    window.location.href = "../../pages/amarte.html";
+  }
+}
